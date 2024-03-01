4eee7bb3837aad779af3710bffcce15654bcbdaf
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2024-03-01 11:12:27 +0530

    scope this and arrow

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,63 @@
+// node scope vs global scope
+// when we run the code with node environment the scope is different 
+// when we run the code with Globe scope which is in console of browser the scope is different form node scope vs global scope
+
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+
+function addone(num){
+    return num + 1
+}
+console.log(addone(5))
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
