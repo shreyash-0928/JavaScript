40a5b5b3252d1cf383204c55ffee2af999effa76
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2024-03-03 18:33:17 +0530

    Data type summary

--- 01_Basics/datatypeSummary.js
+++ 01_Basics/datatypeSummary.js
@@ -77,3 +77,30 @@ let userTwo = userOne
 userTwo.email = "shreyash@gmail.com"
 console.log(userTwo.email);
 console.log(userOne.email);
+
+
+
+// It store string data type
+let txt = "GeeksforGeeks";
+
+// It store integer data type
+let a = 5;
+let b = 5;
+
+// It store Boolean data type
+(a == b )
+
+// To check Strictly (i.e. Whether the datatypes
+// of both variables are same) === is used
+(a === b) // --->  returns true to the console
+ 
+// It store array data type
+let places= ["GFG", "Computer", "Hello"];
+
+// It store object data (objects are 
+// represented in the below way mainly)
+let Student = {
+firstName:"Johnny",
+ lastName:"Diaz", 
+ age:35, 
+ mark:"blueEYE"}
---
+++ 04_control_flow/index.js
@@ -0,0 +1,3 @@
+var string1 = "Code";
+var string2 = "Chef";
+console.log(string1 + string2);
