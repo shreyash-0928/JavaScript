3df88cec9c67e9f20a5b5ff278c4d0b93f344567
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2024-04-03 21:45:04 +0530

    JS ES6 learning

---
+++ 11_jsES6/script.js
@@ -0,0 +1,6 @@
+// let and const dono different hai, mtlb let makes a variables and const makes a constant 
+// let and const makes a constant
+// let and const are similar in few things : 
+// let and const dono es6 se hai
+// let and const dono khudko windo object mein add krte hai
+// .................. braces scoped hai
